1c28af4f6073fbb70e7f31f1096ab4ee4dafd468
David Khourshid <davidkpiano@gmail.com>
2017-11-24 19:52:54 -0500

    Adding missing files

---
+++ src/nm8.ts
@@ -0,0 +1,34 @@
+// Get it? An-im-a-tion?
+export interface Nm810 {
+  play: () => void
+  pause: () => void
+}
+
+const nm8 = (fn: (deltaOrOffset: number) => any, duration: number): Nm810 => {
+  let rate = 0
+  let elapsed = 0
+  let currentTime: number
+  let resetCurrentTime = () => (currentTime = performance.now())
+  let tick = () => {
+    let delta = -currentTime + resetCurrentTime()
+    elapsed += delta
+    fn(duration ? elapsed / duration : delta)
+    return !rate || elapsed >= +duration || requestAnimationFrame(tick)
+  }
+  let nm810 = {
+    play: () => (
+      (rate = 1),
+      elapsed >= +duration && (elapsed = 0),
+      resetCurrentTime(),
+      tick(),
+      nm810
+    ),
+    pause: () => ((rate = 0), nm810),
+    stop: () => (
+      (elapsed = duration || 1 / 0), resetCurrentTime(), tick(), nm810
+    )
+  }
+  return nm810
+}
+
+export default nm8
